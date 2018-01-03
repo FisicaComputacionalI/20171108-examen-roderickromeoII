b7214986fdc32d56af1288d81e107c9174598a2b
Isabel Pedraza <isapedraza@hotmail.com>
2018-01-03 08:01:36 +0100

    calificacion del examen

--- diagramadflujo.cc
+++ diagramadflujo.cc
@@ -8,7 +8,9 @@ int main(){
  
   int x=1; //automaticamente la entrada es 1
  
-  do{while(x<=5)
+  //Este do hace que tu programa nunca se cierre. 
+  //do{
+    while(x<=5)
      if ((x%3)==0){ 
 	x=x+2;
     }
@@ -16,9 +18,11 @@ int main(){
       cout<< x <<endl;
       x=x+2; 
     }
- }while(x>5);
+    //}while(x>5);
     
   return 0;
 }
+//Falta indicar porque solo imprime 1 y 5, y no 1,3 y 5.
+
 //do inicia la corrida si x es menor o igual que 5, despues verificamos si x es divisible entre 3, sino muestra el numero que es divisible y para cualquier caso, se establece un nuevo x como el anterior x mas 2 y se repite el ciclo hasta que x mayor a 5
 //en si las salidas son 1 y 5
